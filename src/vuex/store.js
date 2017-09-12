24d09a5776fc3da791c9e6b65950056380a04f01
胡志甫 <banjuanshutong@126.com>
2017-09-12 18:39:32 +0800

    多余内容删减

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 
-import com from './modules/com';
 import test from './modules/test';
 import user from './modules/user';
 
@@ -11,7 +10,6 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        com,
         test,
         user
     }
--- src/vuex/types.js
+++ src/vuex/types.js
@@ -1,15 +1,3 @@
-//公共
-export const COM_NAV_STATUS     = 'COM_NAV_STATUS'
-export const COM_HEADER_STATUS  = 'COM_HEADER_STATUS'
-export const COM_LOADING_STATUS = 'COM_LOADING_STATUS'
-
-export const COM_SHOW_TOAST = 'COM_SHOW_TOAST'              //显示toast
-export const COM_SHOW_SUCCESS = 'COM_SHOW_SUCCESS'          //显示成功toast
-export const COM_SHOW_FAIL = 'COM_SHOW_FAIL'                //显示失败toast
-export const COM_TOAST_MSG = 'COM_TOAST_MSG'                //显示toast文字
-export const COM_SHOW_ALERT = 'COM_SHOW_ALERT'
-export const COM_ALERT_MSG = 'COM_ALERT_MSG'
-export const COM_SHOW_TIME_PICKER = 'COM_SHOW_TIME_PICKER'  //显示timepicker
 
 
 //测试模块
